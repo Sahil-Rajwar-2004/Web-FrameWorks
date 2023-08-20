94533490d9713c5a0e1a40bd0ec839715ffec897
Sahil-Rajwar <sahilrajwar2004@gmail.com>
2023-08-20 19:01:47 +0530

    display-name using Vite+Svelte

---
+++ display-name/src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+  let name = "World"
+
+  function update(event){
+    name = event.target.value;
+  }
+
+  function reset(){
+    name = "World"
+  }
+</script>
+
+<main>
+  <h1>Hello <span id="name">{name}</span>!</h1>
+  <input placeholder="name" on:input={update}/>
+  <button on:click={reset}>Reset</button>
+</main>
+
+<style>
+  input{
+    min-width: 15rem;
+    min-height: 2rem;
+  }
+
+  span{
+    color: orange;
+    text-transform: capitalize;
+    user-select: none;
+    cursor: pointer;
+    text-decoration: underline white;
+    transition: text-decoration 0.6s ease-in-out;
+  }
+
+  span:hover{
+    text-decoration: underline dodgerblue;
+  }
+
+  button{
+    min-width: 5rem;
+    min-height: 2rem;
+  }
+
+  button:hover{
+    box-shadow: dodgerblue;
+  }
+</style>
