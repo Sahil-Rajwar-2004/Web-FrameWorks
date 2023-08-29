b6cfbdd0464afc811b1d272f061e1fe2feed1a80
Sahil-Rajwar <sahilrajwar2004@gmail.com>
2023-08-29 23:35:28 +0530

    draw canvas

---
+++ draw/src/App.jsx
@@ -0,0 +1,9 @@
+import DrawCanvas from "./components/Canvas"
+
+function App() {
+  return (
+    <DrawCanvas width={500} height={450}/>
+  )
+}
+
+export default App
---
+++ draw/src/components/Canvas.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useRef } from 'react';
+import "./canvas.css"
+
+const DrawCanvas = ({width,height}) => {
+  const canvasRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  const handleMouseDown = (event) => {
+    setIsDrawing(true);
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    context.beginPath();
+    context.moveTo(
+      event.clientX - canvas.offsetLeft,
+      event.clientY - canvas.offsetTop
+    );
+  };
+
+  const handleMouseMove = (event) => {
+    if (isDrawing) {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext('2d');
+      context.lineTo(
+        event.clientX - canvas.offsetLeft,
+        event.clientY - canvas.offsetTop
+      );
+      context.stroke();
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDrawing(false);
+  };
+
+  const handleMouseLeave = () => {
+    setIsDrawing(false);
+  };
+
+  const handleEraseClick = () => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  };
+
+  const handleSaveClick = () => {
+    const canvas = canvasRef.current;
+    const image = canvas.toDataURL('image/png');
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'drawing.png';
+    link.click();
+  };
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>Draw somethin' within the box 👇</h1>
+      </div>
+      <canvas
+        id="draw"
+        ref={canvasRef}
+        width={width}
+        height={height}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseLeave}
+      ></canvas>
+      <div className="btn">
+        <button id="eraseBtn" onClick={handleEraseClick}>
+          Erase
+        </button>
+        <button id="saveBtn" onClick={handleSaveClick}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DrawCanvas;
